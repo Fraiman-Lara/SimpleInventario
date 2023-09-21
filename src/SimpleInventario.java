77e005b90bfc19db70bd8df3e9b4322946184b0e
Laboratorio 109 Pc-19 <lab-109-inf-19>
2023-09-21 12:28:41 -1100

    Nueva clase agregada

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SimpleInventario {
+public class SimpleInventario {
+
+
+    public static void agregarArticulo(String s){
+
+    }
+
+    public static void removerArticulo(int id){
+
+    }
+    public static double obtenerPrecioArticulo(int id){
+        return  0.00;
+
+    }
+    public static  void imprimirInventario(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese una opcion");
+
+    }
+
 }
